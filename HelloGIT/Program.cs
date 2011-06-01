4fa2daef1662212c73592c68fa3b896754de651a
Ian Harrington <toby@softwaresystems.ae>
2011-06-01 13:32:54 +0100

    Removed Bye

--- HelloGIT/Program.cs
+++ HelloGIT/Program.cs
@@ -10,7 +10,6 @@ namespace HelloGIT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Jobin");
-            Console.WriteLine("Bye 12");
             Console.ReadLine();
         }
     }
